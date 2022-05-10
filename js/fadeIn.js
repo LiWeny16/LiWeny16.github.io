855a99186fa50255840b109ef3654d82bd6ea972
LiWeny16 <454888395@qq.com>
2022-05-11 02:09:56 +0800

    Update fadeIn.js

--- js/fadeIn.js
+++ js/fadeIn.js
@@ -1,9 +1,56 @@
 'use strict';
-$(".card").click(
+$(".card_outside").click(
     function fadein() {
-        $(".card").fadeOut(1600);
+        $(".card_outside").fadeOut(1600);
+		document.getElementById("bgmusic").volume=0.3;
+		document.getElementById("bgmusic").play();
+		document.getElementById("stmusic").play();
+		rian_now();
     }
 );
+function rian_now(){
+    const box=document.getElementById('rainBox');
+    let boxHeight=box.clientHeight;
+    let boxWidth=box.clientWidth;
+    // 页面大小发生变化时，改变盒子大小
+    window.onresize=function(){
+        boxHeight=box.clientHeight;
+        boxWidth=box.clientWidth;
+    }
+    // window.onload=function(){
+    //     setTimeout(function(){
+    //     if(document.getElementById("bgmusic").pause())
+    //     {
+    //         document.getElementById("bgmusic").play()
+    //     }}
+    //     ,"1000")
+       
+    // }
+  
+    // 每隔一段时间,添加雨滴
+   setInterval(function(){
+    const rain=document.createElement('div');
+    rain.classList.add('rain');
+    rain.style.top=0;
+    // 随机刷新雨点位置
+    rain.style.left=Math.random()*boxWidth+'px';
+    // 随机雨点透明度
+    rain.style.opacity=Math.random();
+    box.appendChild(rain);
+    // 每隔一段时间,雨水下落
+    let race=1;
+    const timer=setInterval(function(){
+        if(parseInt(rain.style.top)>boxHeight){
+            clearInterval(timer);
+            box.removeChild(rain);
+        }
+        race++;
+        rain.style.top=parseInt(rain.style.top)+race+'px'
+    },20)
+   },50)
+  
+}
+
 function setCookie(cname,cvalue,exdays){
 	var d = new Date();
 	d.setTime(d.getTime()+(exdays*24*60*60*1000));
