75edb2e5be59d485a723604dc9f101bc99bcf552
LiWeny16 <454888395@qq.com>
2022-05-23 04:00:25 +0800

    'update'

--- js/bg.js
+++ js/bg.js
@@ -1,2 +1,13 @@
 
-document.getElementsByClassName("bg")[0].style.background="url(background/back2.jpg)"
+// document.getElementsByClassName("bg")[0].style.background="url(background/back2.jpg)"
+function randomColor() {//得到随机的颜色值
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+
+var RGB=randomColor();
+console.log(RGB);
+
+document.getElementById("secondPage").style.background=`linear-gradient(to right top, #ffffff, ${RGB})`
