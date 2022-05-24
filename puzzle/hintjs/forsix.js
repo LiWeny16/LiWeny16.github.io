0f8d7020f1d7c6394165781d2c07847de41094bc
LiWeny16 <454888395@qq.com>
2022-05-25 00:35:48 +0800

    'update'

--- puzzle/hintjs/forsix.js
+++ puzzle/hintjs/forsix.js
@@ -4,7 +4,7 @@ document.addEventListener('keydown',function(e){    //对整个页面监听
     var anser=document.getElementById("input").value
     if(e.key=='Enter'){  
         if(anser=="柯南"||anser=="工藤新一"||anser=="洗衣机"){
-            alert('Congratulations!');
+            // alert('Congratulations!');
             window.location.href="https://bigonion.xyz/puzzle/level7"; 
         }
     }  
