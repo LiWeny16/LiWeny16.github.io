0cb12c63328194bfc3fbbda7881f41829b1499f5
LiWeny16 <454888395@qq.com>
2022-10-22 21:16:24 +0800

    2333

--- php/ajax.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$name = $_POST['name'];
-$pwd = $_POST['pwd'];
-$array = array("$name","$pwd");
-//这里进行一个些操作，比如数据库交互
- 
-echo json_encode($array);//json_encode方式是必须的
-?>
--- puzzle/hintjs/forseven.js
+++
@@ -1,6 +0,0 @@
-// var a=decodeURI("%E6%88%91%E4%B8%8D%E7%9F%A5%E9%81%93");
-// var b=window.atob("ZmFsc2U=");
-// var url=window.atob("aHR0cHM6Ly9iaWdvbmlvbi54eXovcHV6emxlL2NvbG9y");
-var a=decodeURI('%E6%88%91%E4%B8%8D%E7%9F%A5%E9%81%93');var b=window['atob']('ZmFsc2U=');var url=window['atob']('aHR0cHM6Ly9iaWdvbmlvbi54eXovcHV6emxlL2NvbG9y');document['addEventListener']('keydown',function(_0x12abd1){var _0x3f1045=document['getElementById']('input')['value'];if(_0x12abd1['key']=='Enter'){if(_0x3f1045=='我不知道'||_0x3f1045==='false'){console['log']('ww');window['location']['href']='https://bigonion.xyz/puzzle/color';}}});
-
-//别看我代码快走开！
